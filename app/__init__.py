e928723257257ec548bbc02933228646f42897bd
Prabingiri <giriprabin90@gmail.com>
2020-11-01 15:44:00 -0600

    Add files via upload

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 # Tip - To install packages from a requirements.txt file, run pip install -r requirements.txt
 if app.config["ENV"] == "production":
     app.config.from_object("config.ProductionConfig")
